be9e7e787bbb645a08ff35b11e37dd89d164459c
SathishaK <99187593+SathishaK@users.noreply.github.com>
2022-02-07 22:59:16 +0530

    Update SumOfPrime.cs

--- Assignments(RIT)/Assignment/SumOfPrime.cs
+++ Assignments(RIT)/Assignment/SumOfPrime.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +8,24 @@ namespace Assignments
 {
     class SumOfPrime
 
-    {        static bool checkPrime(int numberToCheck)        {            if (numberToCheck == 1)            {                return false;            }            for (int i = 2;i * i <= numberToCheck; i++)            {                if (numberToCheck % i == 0)                {                    return false;                }            }            return true;        }        static int primeSum(int s, int k)        {
+    {
+        static bool checkPrime(int numberToCheck)
+        {
+            if (numberToCheck == 1)
+            {
+                return false;
+            }
+            for (int i = 2;i * i <= numberToCheck; i++)
+            {
+                if (numberToCheck % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static int primeSum(int s, int k)
+        {
             int sum = 0;
             for (int i = k; i >= s; i--)
 
@@ -20,7 +37,18 @@ namespace Assignments
                 }
             }
             return sum;
-        }        static void Main(string[] args)        {            int s = 10, k = 20;            Console.Write("enter from value:");
-            s = Convert.ToInt32(Console.ReadLine());            Console.Write("enter To value:");
-            k = Convert.ToInt32(Console.ReadLine());            Console.WriteLine("sum of prime numbers");            Console.Write(primeSum(s, k));            Console.ReadLine();        }    }
+        }
+        static void Main(string[] args)
+        {
+            int s = 10, k = 20;
+            Console.Write("enter from value:");
+            s = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter To value:");
+            k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("sum of prime numbers");
+            Console.Write(primeSum(s, k));
+            Console.ReadLine();
+
+        }
+    }
 }
