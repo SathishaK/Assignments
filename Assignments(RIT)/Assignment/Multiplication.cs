5180a5d5db4d704b2bd7706962ea61e0212a2224
SathishaK <99187593+SathishaK@users.noreply.github.com>
2022-02-07 22:44:09 +0530

    Add files via upload

---
+++ Assignments(RIT)/Assignment/Multiplication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace day1
+{
+    class Multiplication
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number:");
+            int a = int.Parse(Console.ReadLine());
+            int i = 1;
+            while (i <= 10)
+            {
+                Console.WriteLine(a + "*" + i + "=" + i * a);
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignments(RIT)/Assignment2/ConvertTabular.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigns
+{
+    class ConvertTabular
+    {
+        static void Main(string[] args)
+        {
+            CsvToDataTable asv = new CsvToDataTable();
+            DataTable dt = asv.ConvertCsvToDataTable(@"C:\Users\Sathisha K\source\repos\Assigns\Assigns\EmpData1.csv");
+            asv.ShowData(dt);
+        }
+
+        class CsvToDataTable
+        {
+            public DataTable ConvertCsvToDataTable(string File)
+            {
+                
+                string[] rows = System.IO.File.ReadAllLines(File);
+
+                DataTable dt = new DataTable();
+                string[] rowValues = null;
+                DataRow dr = dt.NewRow();
+
+                
+                if (rows.Length > 0)
+                {
+                    foreach (string columnName in rows[0].Split(','))
+                        dt.Columns.Add(columnName);
+                }
+
+                
+                for (int row = 1; row < rows.Length; row++)
+                {
+                    rowValues = rows[row].Split(',');
+                    dr = dt.NewRow();
+                    dr.ItemArray = rowValues;
+                    dt.Rows.Add(dr);
+                }
+
+                return dt;
+            }
+
+            public void ShowData(DataTable dt)
+            {
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    foreach (DataColumn dc in dt.Columns)
+                    {
+                        Console.Write(dc.ColumnName + " ");
+                    }
+                    Console.WriteLine("\n___________________");
+
+                    foreach (DataRow draw in dt.Rows)
+                    {
+                        foreach (var item in draw.ItemArray)
+                        {
+                            Console.Write(item.ToString() + "      ");
+                        }
+                        Console.Write("\n");
+                    }
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
+
